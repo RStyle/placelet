489975a293d757e7736a0dc55b39a22eb5100492
RStyle <rsty96@googlemail.com>
2013-08-28 21:55:29 +0200

    Hinzufügen der functions.php Datei

--- index.php
+++ index.php
@@ -1,7 +1,26 @@
 <?php
-echo'
-Seite:<br/>
-'.$page.'
+include_once('./other/functions.php'); //Einbinden einer Datei, welche verschiedene PHP-Funktionen bereitstellt, wie z.B. eine Überprüfung, ob die hochgeladene Datei wirklich ein Bild ist
+//Hier werden Cookies überprüft gesetzt usw.
+
+//--//
+$title = 'Pracelet';  // Kann von anderen Dateien geändert werden, z.B. in "Pracelet Shop"
+
+
+
+//Ich denke, dass der Head immer gleich sein wird, auf Wunsch kann das aber geändert werden
+echo'<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="description" content="Placelet shop and image service">
+<meta name="keywords" content="Placelet, Placelet Shop, Global Bracelet, Travel & Connect, Global Bracelet. Travel & Connect, Travel and Connect, Global Bracelet. Travel and Connect">
+<meta name="author" content="Roman S., Danial S., Julian Z.">
+<title>'.$title.'</title>
+</head>
+<body>
 ';
-print_r($_GET);
+include_once('./pages/'.$page.'.php');
+
+echo'</body>
+</html>';
 ?>
