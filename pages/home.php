4d93074de3b6088e4706b9014140edae1368d971
zimmerjuli97 <zim.julian@gmail.com>
2013-10-16 13:03:59 +0200

    Aufbau-Hinweis auf der Startseite

--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,12 @@
+        <div class="hint">
+            <h1>Hinweis:</h1>
+            <p>
+                Sehr geehrter Besucher,<br><br>
+                Sie befinden sich hier auf der Webseite des JUNIOR-Unternehmens "Placelet". Bitte beachten Sie, dass sich diese Webseite noch im Aufbau befindet und daher einige unvollst&auml;ndige Inhalte wie zum Beispiel Platzhalter-Texte enthalten k&ouml;nnte. Wir arbeiten an einer z&uuml;gigen Vervollst&auml;ndigung dieser Webseite und bitten um Ihr Verst&auml;ndnis. <br> <br>
+                Ihr Placelet-Team
+            </p>   
+        </div>
+        
         <article id="reisearmband" class="mainarticles bottom_border_blue">
 			<div class="blue_line mainarticleheaders line_header"><h1>Reisearmband</h1></div>
 			<img id="reisearmband_img" alt="Reisearmband" src="http://1.bp.blogspot.com/-zjwIgMkzOjY/UY0Go8XBceI/AAAAAAAAFH0/rC73lAYTAUg/s1600/armband+ganz+leicht+selber+machen.jpg">
@@ -25,7 +34,7 @@
 			Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
 			et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
 			Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-		</article>
+		</article>                                                                                                        
 		
 		<article id="kollektion" class="mainarticles bottom_border_green">
 			<div class="green_line mainarticleheaders line_header"><h1>feste Kollektion</h1></div>
@@ -39,7 +48,7 @@
 		</article>
 		<aside class="side_container">
 			<h1>JUNIOR</h1>
-			<p>JUNIOR ist ein Projekt der Institut der deutschen Wirtschaft Köln JUNIOR gGmbH. JUNIOR wird 
+			<p>Das Unternehmen Placelet entstand durch das Projekt JUNIOR der Institut der deutschen Wirtschaft Köln JUNIOR gGmbH. JUNIOR wird 
 			auf Bundesebene durch das Bundesministerium für Wirtschaft und Technologie, die KfW Mittelstandsbank, Gesamtmetall, dem Handelsblatt, Danfoss, Deloitte, der AXA Versicherung und Fed 
 			Ex gefördert.</p>
 		</aside>
