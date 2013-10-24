61eee61422ebf3ad85cc62709614120ceee47d17
zimmerjuli97 <zim.julian@gmail.com>
2013-10-24 18:55:59 +0200

    Kontaktformular Design

--- pages/kontakt.php
+++ pages/kontakt.php
@@ -1,4 +1,24 @@
         <article id="kontakt" class="mainarticles bottom_border_green">
             <div class="green_line mainarticleheaders line_header"><h1>Kontakt</h1></div>
-            Kontakformular wird folgen.
+            <p>Nutzen sie dieses Formular, wenn sie Fragen zu unseren Produkten oder Anliegen bez&uuml;glich unserer Webseite haben. <br>
+                Wir bem&uuml;hen uns, ihnen schnellstm&ouml;glich eine Antwort per E-Mail zukommen zu lassen.</p>
+            <form style="padding-left: 20px; border-left: 4px #BEBEBE solid;">
+                  <label for="absender">Ihre E-Mail-Adresse:</label>
+                  <input type="text" name="absender">
+                
+                  <p>Bitte geben sie den Betreff ihrer Nachricht an:</p>
+                  <p>
+                    <input type="radio" name="Zahlmethode" value="support"> Unsere Webseite<br>
+                    <input type="radio" name="Zahlmethode" value="info"> Unser Produkt<br>
+                    <input type="radio" name="Zahlmethode" value="info"> Anderes<br>
+                  </p>
+                  
+                  <label for="nachricht">Ihre Nachricht:</label>    <br>
+                  <textarea name="nachricht" cols="120" rows="10"></textarea><br><br> 
+                  
+                  <label for="absender">Captcha</label>
+                  <input type="text" name="absender"><br> <br>
+                  
+                  <input type="submit" value="Abschicken">
+            </form>
         </article>
