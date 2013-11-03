3c48710e8361514ec7130a3225254efb51eddee4
zimmerjuli97 <zim.julian@gmail.com>
2013-11-03 16:21:50 +0100

    "Connect-Leiste" Strukturierung

--- pages/home.php
+++ pages/home.php
@@ -1,11 +1,25 @@
-			<div class="hint" id="hint1">
+<!--HINWEIS-->
+            <div class="hint" id="hint1">
 				<h1>Hinweis:</h1>
 				<p>
 					Sie befinden sich hier auf der Webseite des JUNIOR-Unternehmens "Placelet". Bitte beachten Sie, dass sich diese Webseite noch im Aufbau befindet und daher einige unvollst&auml;ndige Inhalte wie zum Beispiel Platzhalter-Texte enthalten k&ouml;nnte. Wir arbeiten an einer z&uuml;gigen Vervollst&auml;ndigung dieser Webseite und bitten um Ihr Verst&auml;ndnis.
 				</p> 
 				<span class="pseudo_link toggle_comments" onclick="document.getElementById('hint1').style.display='none';">Hinweis ausblenden</span>  
 			</div>
+<!--CONNECT-LEISTE-->
+            <div id="connect_leiste">
+                <div class="connect_box" id="uploads_box">
+                    <h1>neueste uploads</h1>    
+                </div>
+                <div class="connect_box" id="submit_box">
+                    <h1>bild posten</h1> 
+                </div>
+                <div class="connect_box" id="facebook_box">
+                    <h1>facebook</h1> 
+                </div>
+            </div>
 
+<!--ERSTER ARTIKEL-->
 			<article id="reisearmband" class="mainarticles bottom_border_blue">
 				<div class="blue_line mainarticleheaders line_header"><h1>Reisearmband</h1></div>
 				<img id="reisearmband_img" alt="Reisearmband" src="http://1.bp.blogspot.com/-zjwIgMkzOjY/UY0Go8XBceI/AAAAAAAAFH0/rC73lAYTAUg/s1600/armband+ganz+leicht+selber+machen.jpg">
@@ -34,12 +48,12 @@
 				et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
 				Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
 			</article>                                                                                                        
-
+<!--ZWEITER ARTIKEL-->
 			<article id="kollektion" class="mainarticles bottom_border_green">
 				<div class="green_line mainarticleheaders line_header"><h1>Wie funktioniert's?</h1></div>
 				<img id="kollektion_img" alt="feste Kollektion" src="http://img.geo.de/div/image/61566/01-armbaender.jpg">
 				<p>
-					<span class="highlighted kollektion_numbers">1</span>Kaufe dir eines unserer Armänder in unserem Shop, Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+					<span class="highlighted kollektion_numbers">1</span>Kaufe dir eines unserer Armbänder in unserem Shop, Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
 					et dolore magna aliquyam er.
 				</p>
 				<span class="arrow highlighted">&#11015;</span>
@@ -53,7 +67,7 @@
 					et dolore magna aliquyam er.
 				</p>
 			</article>
-
+<!--SIDEBAR-->
 			<aside class="side_container">
 				<h1>JUNIOR</h1>
 				<p>Das Unternehmen Placelet entstand durch das Projekt JUNIOR der Institut der deutschen Wirtschaft Köln JUNIOR gGmbH. JUNIOR wird 
