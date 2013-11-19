5b47745b2de5a6ef2a31e73db078c11975ab714f
JohnAZoidberg <ds@struckmeierfliesen.de>
2013-11-19 19:36:42 +0100

    Merging!

--- scripts/ajax/ajax_start.php
+++ scripts/ajax/ajax_start.php
@@ -106,11 +106,7 @@ echo'<!--HR über dem 1. nachgeladenen Bild--><hr style="clear: both;">';
 					$last_comment = 'last';
 				}
 ?>
-<<<<<<< HEAD
 					<a href="start?last_comment=<?php echo $last_comment; ?>&commid=<?php echo $stats[$i][0][$j]['commid']; ?>&picid=<?php echo $stats[$i][0][$j]['picid']; ?>&comm_name=<?php echo urlencode($statistics->brid2name($bracelets_displayed[$i])); ?>&delete_comm=true" class="delete_button float_right">X</a>
-=======
-					<a href="http://placelet.de/start?commid=<?php echo $stats[$i][0][$j]['commid']; ?>&picid=<?php echo $stats[$i][0][$j]['picid']; ?>" class="delete_button float_right">X</a>
->>>>>>> 9ad200d9d563443e2bef126dfe1e748fd97cce57
                     <strong><?php echo $stats[$i][0][$j]['user']; ?></strong>, <?php echo $x_days_ago.' ('.date('H:i d.m.Y', $stats[$i][0][$j]['date']).')'; ?>
                     <p><?php echo $stats[$i][0][$j]['comment']; ?></p> 
                     <hr style="border: 1px solid white;">  
