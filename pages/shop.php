cef95b24d245f0557c7e0ba761f39ca58fbc020c
RStyle <rsty96@googlemail.com>
2014-01-09 22:06:43 +0100

    PayPalButton

--- pages/shop.php
+++ pages/shop.php
@@ -32,6 +32,12 @@
 					<hr>
 					<h4>Geschlecht:</h4>
 					Unisex ( &#x2642; | &#x2640; )<br>
-					<a class="order_button" href="order">Jetzt Bestellen!</a>
+					<!--<a class="order_button" href="order">Jetzt Bestellen!</a>-->
+					<br><form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+						<input type="hidden" name="cmd" value="_s-xclick">
+						<input type="hidden" name="hosted_button_id" value="AWD5N67HHG896">
+						<input type="image" src="https://www.paypalobjects.com/de_DE/DE/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="Jetzt einfach, schnell und sicher online bezahlen – mit PayPal.">
+						<img alt="" border="0" src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" width="1" height="1">
+					</form>
 				</div>
 			</article>
