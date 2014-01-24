5ac5d3b37255a9def80bfbd0018d9af0ce3dad91
zimmerjuli97 <zim.julian@gmail.com>
2014-01-24 21:53:41 +0100

    Mehr Fragen im FAQ

--- pages/faq.php
+++ pages/faq.php
@@ -16,8 +16,24 @@
 				<div class="answers" id="answer_2">
 					<p>
 						Auch wenn du auch ohne Account bestimmte Funktionen unserer Webseite nutzen kannst, bringt dir ein eigener Account einige Vorteile.
-						Er ist die Vorraussetzung Du erhältst zum Beispiel eine eigene Profilseite, anhand derer deine Freunde deine Armbänder und Uploads nachverfolgen können.
-						Außerdem wirst diesen unschönen Gast-Tag vor allen deinen Kommentaren los.
+						Er ist zum Einen erforderlich, um selbst ein Armband um die Welt schicken zu können. Du erhältst außerdem eine eigene Profilseite, anhand derer deine Freunde deine Armbänder und Uploads nachverfolgen können und du wirst diesen unschönen Gast-Tag vor allen deinen Kommentaren los.
+					</p>
+				</div>
+				
+				<p class="questions pseudo_link" id="question_3"><span class="question_arrow3 arrow_right"></span>&nbsp;Wie können meine Freunde den Weg meines Armbandes verfolgen?</p>
+				<hr style="margin-top: 0; height: 3px; background-color: #ddd; border: none;">
+				<div class="answers" id="answer_3">
+					<p>
+						Generell kannst Du dein Armband überall mithilfe eines permanenten Links verbreiten ("www.placelet.de/[Dein Armbandname]"). Zudem kann das Armband über die Suchleiste mit dem Armbandnamen, den du selbst festlegen kannst, gefunden werden.
+						Registrierte Benutzer haben auch die Möglichkeit, dein Armband zu abonnieren und so automatisch über neue Bilder zu diesem Armband informiert zu werden.
+					</p>
+				</div>
+				
+				<p class="questions pseudo_link" id="question_4"><span class="question_arrow4 arrow_right"></span>&nbsp;Was kann ich tun, wenn ich die ID eines meiner Armbänder vergessen habe?</p>
+				<hr style="margin-top: 0; height: 3px; background-color: #ddd; border: none;">
+				<div class="answers" id="answer_4">
+					<p>
+						Gehe zu deinem Profil und bewege den Mauszeiger über das Armband. Es wird ein Feld mit der ID erscheinen.
 					</p>
 				</div>
 			</article>
