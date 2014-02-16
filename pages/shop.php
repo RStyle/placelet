c51fada5ee12ea1a95f12c864f7a78318b845ff2
RStyle <rsty96@googlemail.com>
2014-02-16 23:47:02 +0100

    ShopPaypalOnline

--- pages/shop.php
+++ pages/shop.php
@@ -28,26 +28,45 @@
 					<h4><?php echo $lang->shop->color[$lng.'-title']; ?></h4>
 					<?php echo $lang->shop->color->$lng; ?><br>
                     <?php echo $lang->shop->claspcolor->$lng; ?> 
-                        <select size="1" name="color">
-                        	<option value="white"><?php echo $lang->shop->white->$lng; ?></option>
-                        	<option value="green"><?php echo $lang->shop->green->$lng; ?></option>
-                        	<option value="blue"><?php echo $lang->shop->blue->$lng; ?></option>
-                        	<option value="red"><?php echo $lang->shop->red->$lng; ?></option>
-                        	<option value="yellow"><?php echo $lang->shop->yellow->$lng; ?></option>
-                        </select><br>             					
+					<select size="1" name="os0" form="paypal">
+						<option value="<?php echo $lang->shop->white->$lng; ?>"><?php echo $lang->shop->white->$lng; ?></option>
+						<option value="<?php echo $lang->shop->green->$lng; ?>"><?php echo $lang->shop->green->$lng; ?></option>
+						<option value="<?php echo $lang->shop->blue->$lng; ?>"><?php echo $lang->shop->blue->$lng; ?></option>
+						<option value="<?php echo $lang->shop->red->$lng; ?>"><?php echo $lang->shop->red->$lng; ?></option>
+						<option value="<?php echo $lang->shop->yellow->$lng; ?>"><?php echo $lang->shop->yellow->$lng; ?></option>
+					</select><br>             					
 					<hr>
 					<h4><?php echo $lang->shop->material[$lng.'-title']; ?></h4>
 					<?php echo $lang->shop->material->$lng; ?>
 					<hr>
 					<h4><?php echo $lang->shop->gender[$lng.'-title']; ?></h4>
-					<?php echo $lang->shop->gender->$lng; ?><br>
-					<!--<a class="order_button" href="order"><?php echo $lang->shop->ordernow->$lng; ?></a>
-					<br><form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+					<?php echo $lang->shop->gender->$lng; ?><br><br>
+					<?php if ($lng == 'de'){ ?>
+					<form action="https://www.paypal.com/cgi-bin/webscr" id="paypal" method="post" target="_top">
 						<input type="hidden" name="cmd" value="_s-xclick">
 						<input type="hidden" name="hosted_button_id" value="AWD5N67HHG896">
-						<?php echo $lang->shop->paypal->$lng; ?>
+						<input type="hidden" name="on0" value="Verschlussfarbe" form="paypal">
+						<!--<table>
+						<tr><td><input type="hidden" name="on0" value="Verschlussfarbe">Verschlussfarbe</td></tr><tr><td><select name="os0">
+							<option value="Weiß">Weiß </option>
+							<option value="Grün">Grün </option>
+							<option value="Blau">Blau </option>
+							<option value="Rot">Rot </option>
+							<option value="Gelb">Gelb </option>
+						</select> </td></tr>
+						</table>-->
+						<input type="image" src="https://www.paypalobjects.com/de_DE/DE/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="Jetzt einfach, schnell und sicher online bezahlen – mit PayPal.">
 						<img alt="" border="0" src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" width="1" height="1">
-					</form>-->
-					<p><?php echo $lang->shop->notavailable->$lng; ?></p>
+					</form>
+
+					<?php }else{ ?>
+					<form action="https://www.paypal.com/cgi-bin/webscr" id="paypal" method="post" target="_top">
+						<input type="hidden" name="cmd" value="_s-xclick">
+						<input type="hidden" name="hosted_button_id" value="NQNXQZKL5GNYN">
+						<input type="hidden" name="on0" value="clasp color">
+						<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+						<img alt="" border="0" src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" width="1" height="1">
+					</form>
+					<?php } //$lang->shop->notavailable->$lng; ?>
 				</div>
 			</article>
