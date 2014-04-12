a5cbff8380b247addae431dc3194dcdecd69eae0
zimmerjuli97 <zim.julian@gmail.com>
2014-04-12 11:33:43 +0200

    FAQ Übersetzung

--- pages/faq.php
+++ pages/faq.php
@@ -1,39 +1,27 @@
 ﻿			<article id="faq" class="mainarticles bottom_border_green">
 				<div class="green_line mainarticleheaders line_header"><h1><?php echo $pagename[$page]; ?></h1></div>
-				<p>Hier findest du einige häufig gestellte Fragen. Wenn dir das nicht weiterhilft, <a href="/kontakt">kontaktiere uns</a>.</p>
-				<p class="questions pseudo_link" id="question_1"><span class="question_arrow1 arrow_right"></span>&nbsp;Wie kann ich unangemessene Bilder oder Kommentare löschen beziehungsweise melden?</p>
+				<p><?php echo $lang->faq->faqinfo->$lng; ?></p>
+				<p class="questions pseudo_link" id="question_1"><span class="question_arrow1 arrow_right"></span>&nbsp;<?php echo $lang->faq->questions->q1->$lng; ?></p>
 				<hr style="margin-top: 0; height: 3px; background-color: #ddd; border: none;">
 				<div class="answers" id="answer_1">
-					<p>
-						Wenn du auf unserer Seite unangemessene oder anstößige Bilder oder Kommentare (zum Beispiel Spam) findest, solltest du diese der Seitenadministration melden, damit sie gelöscht werden.
-						Klicke dazu auf den Button mit dem "X" rechts oben neben dem Bild oder Kommentar.<br>
-						Als Inhaber eines Armbandes hast du zudem immer die Möglichkeit, Bilder oder Kommentare über denselben Button direkt zu löschen.
-					</p>
+					<p><?php echo $lang->faq->answers->a1->$lng; ?></p>
 				</div>
 				
-				<p class="questions pseudo_link" id="question_2"><span class="question_arrow2 arrow_right"></span>&nbsp;Warum sollte ich mich auf dieser Seite registrieren?</p>
+				<p class="questions pseudo_link" id="question_2"><span class="question_arrow2 arrow_right"></span>&nbsp;<?php echo $lang->faq->questions->q2->$lng; ?></p>
 				<hr style="margin-top: 0; height: 3px; background-color: #ddd; border: none;">
 				<div class="answers" id="answer_2">
-					<p>
-						Auch wenn du auch ohne Account bestimmte Funktionen unserer Webseite nutzen kannst, bringt dir ein eigener Account einige Vorteile.
-						Er ist zum Einen erforderlich, um selbst ein Armband um die Welt schicken zu können. Du erhältst außerdem eine eigene Profilseite, anhand derer deine Freunde deine Armbänder und Uploads nachverfolgen können und du wirst diesen unschönen Gast-Tag vor allen deinen Kommentaren los.
-					</p>
+					<p><?php echo $lang->faq->answers->a2->$lng; ?></p>
 				</div>
 				
-				<p class="questions pseudo_link" id="question_3"><span class="question_arrow3 arrow_right"></span>&nbsp;Wie können meine Freunde den Weg meines Armbandes verfolgen?</p>
+				<p class="questions pseudo_link" id="question_3"><span class="question_arrow3 arrow_right"></span>&nbsp;<?php echo $lang->faq->questions->q3->$lng; ?></p>
 				<hr style="margin-top: 0; height: 3px; background-color: #ddd; border: none;">
 				<div class="answers" id="answer_3">
-					<p>
-						Generell kannst Du dein Armband überall mithilfe eines permanenten Links verbreiten ("www.placelet.de/[Dein Armbandname]"). Zudem kann das Armband über die Suchleiste mit dem Armbandnamen, den du selbst festlegen kannst, gefunden werden.
-						Registrierte Benutzer haben auch die Möglichkeit, dein Armband zu abonnieren und so automatisch über neue Bilder zu diesem Armband informiert zu werden.
-					</p>
+					<p><?php echo $lang->faq->answers->a3->$lng; ?></p>
 				</div>
 				
-				<p class="questions pseudo_link" id="question_4"><span class="question_arrow4 arrow_right"></span>&nbsp;Was kann ich tun, wenn ich die ID eines meiner Armbänder vergessen habe?</p>
+				<p class="questions pseudo_link" id="question_4"><span class="question_arrow4 arrow_right"></span>&nbsp;<?php echo $lang->faq->questions->q4->$lng; ?></p>
 				<hr style="margin-top: 0; height: 3px; background-color: #ddd; border: none;">
 				<div class="answers" id="answer_4">
-					<p>
-						Gehe zu deinem Profil und bewege den Mauszeiger über das Armband. Es wird ein Feld mit der ID erscheinen.
-					</p>
+					<p><?php echo $lang->faq->answers->a4->$lng; ?></p>
 				</div>
 			</article>
