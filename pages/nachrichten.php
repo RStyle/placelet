9e5a18e680a1aacfcc8190eec125c187c865ec84
zimmerjuli97 <zim.julian@gmail.com>
2014-04-12 20:08:02 +0200

    Nachrichten Design

--- pages/nachrichten.php
+++ pages/nachrichten.php
@@ -1,4 +1,43 @@
 		<article id="recent_pics" class="mainarticles bottom_border_green">
 			<div class="green_line mainarticleheaders line_header"><h2 id="pic_br_switch" data-recent_brid_pics="false"><?php echo $lang->nachrichten[$lng.'-title']; ?></h2></div>
-			<p><?php var_dump($messages); $user->messages_read();?></p>
+			<!--<p><?php var_dump($messages); $user->messages_read();?></p>-->
+			<div id="sidebar">
+			    <ul id="chat_list">
+			        <hr>
+                    <a href="#" style="text-decoration: none;"><li class="selected">
+                         <strong>Jules</strong><br>
+                         <p style="color: #999; margin: 0;">vor 2 Tagen</p>                         
+                    </li></a>
+                    <hr>
+                    <a href="#" style="text-decoration: none;"><li>
+                         <strong>JohnAYoloberg</strong><br>
+                         <p style="color: #999; margin: 0;">vor 3 Tagen</p>
+                    </li></a>
+                    <hr>
+                    <a href="#" style="text-decoration: none;"><li>
+                         <strong>JohnASwagberg</strong><br>
+                         <p style="color: #999; margin: 0;">vor 23 Stunden</p>
+                    </li></a>
+                    <hr>
+                </ul> 
+            </div>
+            <div id="chat_room">
+                <div id="message_box">
+                    <div class="post">
+                        <img src="/img/profil_pic_small.png" width="40" style="border: 1px #999 solid; float: left; margin-right: 10px;">
+                        <div style="float: left;"><p style="color: #999; margin: 0;"><strong style="color: #b7d300">Jules</strong>, vor 2 Minuten</p>
+                        <p style="margin: 5px;">Yoadawdaw, bitcheeees<br> Was geeeeht???</p></div> 
+                    </div>
+                    <div class="post">
+                        <img src="/img/profil_pic_small.png" width="40" style="border: 1px #999 solid; float: left; margin-right: 10px;">
+                        <div style="float: left;"><p style="color: #999; margin: 0;"><strong style="color: #b7d300">Ich</strong>, vor einer Minute</p>
+                        <p style="margin: 2px;">HaldieFress!<br> SAUFEEEEEEEEEEEEEEEEEEEEEEEN</p></div>
+                    </div>
+                    <p style="color: #999; margin-bottom: 20px;"> *Gesehen (20:34)</p>
+                    <div>
+                        <textarea class="chat_text" placeholder="Antwort verfassen..."></textarea>
+                    </div>
+                </div>
+            </div>
+            
 		</article>
