3b0020ccde59512b22176ee5dc0fbe05385c0bd3
zimmerjuli97 <zim.julian@gmail.com>
2014-05-23 17:47:30 +0200

    Presse Seiten

---
+++ pages/press.php
@@ -0,0 +1,9 @@
+			<article id="impressum" class="mainarticles bottom_border_green">
+				<div class="green_line mainarticleheaders line_header"><h1><?php echo $lang->press[$lng.'-title']; ?></h1></div>
+			    <div class="press_article">
+                    <div class="press_pic" style="clear: both; float: left; background-color: #5CFA73; width:250px; height: 250px; "></div>
+                    <div class="press_header"><p>Placelet holt ersten Platz im Landeswettbewerb</p></div>
+                    <hr class="trenner">
+                    <div class="press_text">&nbsp; <p style="margin: 0;">Lorem adiwen eafiaojfd aefjk adbabefbke sjgh sskaLorem adiwen eafiaojfd aefjk adbabefbke sjgh sskaLorem adiwen eafiaojfd aefjk adbabefbke sjgh sska</p></div>
+                </div>
+			</article>
