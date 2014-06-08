35f075c1b67303cd3786b0ba5fed9ff5c7865c2b
zimmerjuli97 <zim.julian@gmail.com>
2014-06-08 21:16:58 +0200

    Neuer Presseartikel

--- pages/press.php
+++ pages/press.php
@@ -1,12 +1,21 @@
 			<article id="impressum" class="mainarticles bottom_border_green">
 				<div class="green_line mainarticleheaders line_header"><h1><?php echo $lang->press[$lng.'-title']; ?></h1></div>
+				<div class="press_article">
+                    <div class="press_pic" style="overflow: hidden; clear: both; float: left; width:250px; height: 250px;"><img style="position: relative; left: -105px; height: 250px;" src="http://www.mwkel.rlp.de/icc/internet/med/e12/e120d810-8422-6415-1917-b455e1df7d18,11111111-1111-1111-1111-111111111111.jpg"></div>
+                    <div class="press_header"><p>Schülerfirma "Placelet": Landessieger beim Wirtschaftswettbewerb "Junior"<span style="font-size: 0.7em; font-style: italic"> - 21.05.2014</span></p></div>
+                    <hr class="trenner" style="border-color: #05A8FF;">
+                    <div class="press_text"><div style="height: 170px; width: 15px; float: left;"></div>
+                    <i style="display: block; margin-bottom: 10px;">Bad Kreuznach – Sie nennen es ein soziales Experiment, hatten eine zeitgemäße Geschäftsidee, haben ihren eigenen Webshop gegründet und den fiktiven Wert ihrer Unternehmensaktie um 40 Prozent im Vergleich zum Ausgabewert gesteigert. Eine Gruppe engagierter Schüler des Gymnasiums am Römerkastell (Röka) in Bad Kreuznach ist mit ihrer eigenen Firma, die sie Placelet genannt hat . . .</i>
+                    <h4 style="margin: 0;"><a target="_blank" href="http://www.rhein-zeitung.de/region/lokales/bad-kreuznach_artikel,-Schuelerfirma-Placelet-Landessieger-beim-Wirtschaftswettbewerb-Junior-_arid,1160039.html#.U5S0zPl_u-2" class="press_link">Rhein-Zeitung</a></h4>
+                    </div>
+                </div>
 			    <div class="press_article">
                     <div class="press_pic" style="overflow: hidden; clear: both; float: left; width:250px; height: 250px;"><img style="position: relative; left: -105px; height: 250px;" src="http://www.mwkel.rlp.de/icc/internet/med/e12/e120d810-8422-6415-1917-b455e1df7d18,11111111-1111-1111-1111-111111111111.jpg"></div>
                     <div class="press_header"><p>Placelet gewinnt JUNIOR-Landeswettbewerb Rheinland-Pfalz<span style="font-size: 0.7em; font-style: italic"> - 21.05.2014</span></p></div>
                     <hr class="trenner">
                     <div class="press_text"><div style="height: 170px; width: 15px; float: left;"></div>
                     <i style="display: block; margin-bottom: 10px;">Mit einer innovativen Geschäftsidee und einer ansprechenden Präsentation hat es die JUNIOR-Firma „Placelet“ des Gymnasiums am Römerkastell in Bad Kreuznach geschafft: Sie gewann mit ihren Reisearmbändern als bestes rheinland-pfälzisches Schülerunternehmen 2014 den diesjährigen JUNIOR-Landeswettbewerb. Durch das Armband vernetzen sich Teilnehmer einer Community und können so den Reiseweg des Armbandes . . .</i>
-                    <h4 style="margin: 0;"><a target="_blank" href="http://www.allgemeine-zeitung.de/lokales/mainz/nachrichten-mainz/ein-band-das-verbindet_14167153.htm" class="press_link">Allgemeine Zeitung</a> | <a target="_blank" href="http://isb.rlp.de/de/presse/presseinformationen/einzelansicht/article/ein-band-das-verbindet-schuelerfirma-placelet-aus-bad-kreuznach-gewinnt-mit-reisearmbaendern" class="press_link">Investitions- und Strukturbank</a> | <a target="_blank" class="press_link" href="http://www.swr.de/landesschau-aktuell/rp/mainz/schuelerfirma-wird-landessieger/-/id=1662/did=13436760/nid=1662/11my9kf/">SWR RP</a></h4>
+                    <h4 style="margin: 0;"><a target="_blank" href="http://www.allgemeine-zeitung.de/lokales/mainz/nachrichten-mainz/ein-band-das-verbindet_14167153.htm" class="press_link">Allgemeine Zeitung</a> | <a target="_blank" href="http://isb.rlp.de/de/presse/presseinformationen/einzelansicht/article/ein-band-das-verbindet-schuelerfirma-placelet-aus-bad-kreuznach-gewinnt-mit-reisearmbaendern" class="press_link">Investitions- und Strukturbank</a> | <a target="_blank" class="press_link" href="http://www.swr.de/landesschau-aktuell/rp/mainz/schuelerfirma-wird-landessieger/-/id=1662/did=13436760/nid=1662/11my9kf/">SWR RP</a> | <a target="_blank" class="press_link" href="http://www.hanzonline.de/de/newsintro_startseite/sch%C3%BClerfirma-%E2%80%9Eplacelet%E2%80%9C-vom-r%C3%B6ka-gewinnt-junior-la_hvhu7rx3.html">hanzonline</a></h4>
                     </div>
                 </div> 
                 <div class="press_article">
