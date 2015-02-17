82921d3b02565dadae663a2a7ed005bcb080ccba
JohnAZoidberg <ds@struckmeierfliesen.de>
2015-02-17 18:35:03 +0100

    Android 1.3.1

--- android/android1.2.4.php
+++ android/android1.2.4.php
@@ -1,5 +1,5 @@
 <?php
-//Android Versions 1.2.4-1.3.0
+//Android Versions 1.2.4-1.3.1
 ini_set('display_errors', true);
 $return = array('notsentlol' => 'dudenotsent', 'test' => 'waslos?');
 include_once('../scripts/connection.php');
